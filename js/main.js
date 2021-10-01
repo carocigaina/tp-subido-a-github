40d48194d259c74bba2500646e4b5fdd5fd0f3fd
carocigaina <carolina.cigaina@davinci.edu.ar>
2021-10-01 19:49:58 -0300

    modificacion

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ function buscarClima(ciudad){
         
         mostrarResult(data);
         guardarResultados('resultados', data);
+        onYouTubeIframeAPIReady();
     }).catch(function(error){
         console.log('Hay un error',error);
     });
@@ -52,7 +53,7 @@ function mostrarResult(data){
     velViento=data.wind.speed;
     descripClima=data.weather[0]["description"];
     iconClima=data.weather[0]["icon"];
-    /*iconClima.id="icon";*/
+    
     
     li1.innerHTML= 'Clima actual: ' + descripClima;
     li2.innerHTML= 'Temperatura máxima: '+ tempMax;
@@ -62,8 +63,68 @@ function mostrarResult(data){
     li6.innerHTML= 'Presión Atmosférica: '+ presion;
     li7.innerHTML= 'TVelocidad del viento: '+ velViento;
     img.src=`http://openweathermap.org/img/wn/${iconClima}@2x.png`;
+/*
+//mostrar video
+    if (iconClima == '01d'){
+        tag.src=`https://www.youtube.com/watch?v=Azt9moHZ8j8`;
+     }else if(iconClima == '02d' || iconClima == '03d' || iconClima == '04d'){
+        tag.src=`https://www.youtube.com/watch?v=XFoczTlvoY4`;
+     }else if(iconClima == '09d' || iconClima == '10d' || iconClima == '11d'){
+        tag.src=`https://www.youtube.com/watch?v=nTAE-uYo6qs`;  
+    }else if(iconClima == '13d'){
+        tag.src=`https://www.youtube.com/watch?v=52QIQ0788Fc`; 
+    }else if(iconClima == '50d'){
+        tag.src=`https://www.youtube.com/watch?v=Jma89WgyOE8`;  
+    };
+    */
 };
 
 function guardarResultados(name, data){
     localStorage.setItem(name, JSON.stringify(data));
 };
+
+//video
+
+ // 2. This code loads the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+
+ tag.src = "https://www.youtube.com/iframe_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+ // 3. This function creates an <iframe> (and YouTube player)
+ //    after the API code downloads.
+ var player;
+ function onYouTubeIframeAPIReady() {
+   player = new YT.Player('player', {
+     height: '360',
+     width: '640',
+     videoId: 'M7lc1UVf-VE' ,
+
+     events: {
+         
+       'onReady': onPlayerReady,
+       'onStateChange': onPlayerStateChange
+     }
+   });
+ }
+
+ // 4. The API will call this function when the video player is ready.
+ function onPlayerReady(event) {
+   event.target.playVideo();
+ }
+
+ // 5. The API calls this function when the player's state changes.
+ //    The function indicates that when playing a video (state=1),
+ //    the player should play for six seconds and then stop.
+ var done = false;
+ function onPlayerStateChange(event) {
+   if (event.data == YT.PlayerState.PLAYING && !done) {
+     setTimeout(stopVideo, 6000);
+     done = true;
+   }
+ }
+ function stopVideo() {
+   player.stopVideo();
+ }       
+        
